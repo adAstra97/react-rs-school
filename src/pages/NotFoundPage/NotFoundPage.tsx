c123573d6e3e040ac5066dd73a6f5e3bfb54111b
Nika Verman <nikaverman@gmail.com>
2025-02-07 21:22:58 +0300

    feat: add 404 page

--- src/pages/NotFoundPage/NotFoundPage.tsx
+++ src/pages/NotFoundPage/NotFoundPage.tsx
@@ -1,13 +1,16 @@
 import { FC } from 'react';
 import { Link } from 'react-router';
+import HouseIcon from '../../assets/icons/house.svg';
 
 const NotFoundPage: FC = () => {
   return (
-    <div className="text-center">
-      <h1 className="text-4xl font-bold text-red-600">404 - Page Not Found</h1>
-      <p className="mt-4">The page you are looking for does not exist.</p>
-      <Link to="/" className="mt-4 inline-block text-blue-500">
-        Go back to Home
+    <div className="text-center flex flex-col items-center justify-center gap-10 min-h-screen">
+      <span className="text-9xl font-bold text-amber-500">404</span>
+      <span className="text-amber-500 text-2xl">
+        The page you are looking for does not exist.
+      </span>
+      <Link to="/">
+        <img src={HouseIcon} alt="home" />
       </Link>
     </div>
   );
